2322f6eeb401f890dc521dacbd44269acafff5e1
Eduardo Passano Chávez <eapch21@gmail.com>
2016-02-21 20:30:52 -0500

    SE MODIFICO

--- Mibanco/app/src/main/java/com/mibanco/myapplication/BusquedaActivity.java
+++ Mibanco/app/src/main/java/com/mibanco/myapplication/BusquedaActivity.java
@@ -1,14 +1,46 @@
 package com.mibanco.myapplication;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class BusquedaActivity extends AppCompatActivity {
+
+    String IP = "http://upcmoviles2016.esy.es";
+    // Rutas de los Web Services
+    String GET_BY_ID = IP + "/obtener_cliente_x_direccion_o_correo.php";
+    ObtenerWebService hiloconexion;
+    EditText nombre;
+    EditText distrito;
+    TextView resultado;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,19 +57,113 @@ public class BusquedaActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        nombre = (EditText)findViewById(R.id.eNombre);
+        distrito = (EditText)findViewById(R.id.eDistrito);
+        resultado = (TextView)findViewById(R.id.resultado);
+
+
     }
     public void pasaraListaClientesActivity(View v) {
         Intent act = new Intent(this, ListaClientesActivity.class);
-
         startActivity(act);
-
     }
+
     public void VolverAInicioActivity(View v) {
-
         Intent act = new Intent(this, InicioActivity.class);
-
         startActivity(act);
+    }
 
+    public void Buscar(View v) {
+        hiloconexion = new ObtenerWebService();
+        String cadenallamada = GET_BY_ID + "?nombre=" + nombre.getText().toString()+"&distrito=" +distrito.getText().toString();
+        hiloconexion.execute(cadenallamada,"2");   // Parámetros que recibe doInBackground
+    }
+
+    public class ObtenerWebService extends AsyncTask<String,Void,String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String cadena = params[0];
+            URL url = null; // Url de donde queremos obtener información
+            String devuelve ="";
+            if(params[1]=="2"){    // consulta por id
+
+                try {
+                    url = new URL(cadena);
+                    Log.i("url", cadena);
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection(); //Abrir la conexión
+                    connection.setRequestProperty("User-Agent", "Mozilla/5.0" +
+                            " (Linux; Android 1.5; es-ES) Ejemplo HTTP");
+                    int respuesta = connection.getResponseCode();
+                    StringBuilder result = new StringBuilder();
+                    if (respuesta == HttpURLConnection.HTTP_OK){
+                        InputStream in = new BufferedInputStream(connection.getInputStream());  // preparo la cadena de entrada
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(in));  // la introduzco en un BufferedReader
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            result.append(line);        // Paso toda la entrada al StringBuilder
+                            Log.i("mi  error en linea", line);
+                            Log.i("mi  error", "[" + result + "]");
+                        }
+                        //Creamos un objeto JSONObject para poder acceder a los atributos (campos) del objeto.
+                        JSONObject respuestaJSON = new JSONObject(result.toString());   //Creo un JSONObject a partir del StringBuilder pasado a cadena
+                        //Accedemos al vector de resultados
+
+                        String resultJSON = respuestaJSON.getString("estado");   // estado es el nombre del campo en el JSON
+                        if (resultJSON=="1"){      // hay un alumno que mostrar
+                            JSONArray alumnosJSON = respuestaJSON.getJSONArray("clientes");   // estado es el nombre del campo en el JSON
+                            for(int i=0;i<alumnosJSON.length();i++) {
+                                devuelve = devuelve + alumnosJSON.getJSONObject(i).getString("codigo") + " " +
+                                        alumnosJSON.getJSONObject(i).getString("nombre") + " " +
+                                        alumnosJSON.getJSONObject(i).getString("apellido_paterno") + " " +
+                                        alumnosJSON.getJSONObject(i).getString("distrito") + "\n";
+                             /*   devuelve = devuelve + respuestaJSON.getJSONObject("clientes").getString("codigo") + " " +
+                                        respuestaJSON.getJSONObject("clientes").getString("nombre") + " " +
+                                        respuestaJSON.getJSONObject("clientes").getString("apellido_paterno");*/
+                            }
+                        }
+                        else if (resultJSON=="2"){
+                            devuelve = "No hay clientes";
+                        }
+
+                    }
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                return devuelve;
+
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onCancelled(String s) {
+            super.onCancelled(s);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+           resultado.setText(s);
+            //super.onPostExecute(s);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
     }
 
 }
--- Mibanco/app/src/main/java/com/mibanco/myapplication/RegistrarSolicitudActivity.java
+++ Mibanco/app/src/main/java/com/mibanco/myapplication/RegistrarSolicitudActivity.java
@@ -1,14 +1,41 @@
 package com.mibanco.myapplication;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class RegistrarSolicitudActivity extends AppCompatActivity {
-
+    String IP = "http://upcmoviles2016.esy.es";
+    // Rutas de los Web Services
+    String INSERT = IP + "/Insertar_Solicitud.php";
+    ObtenerWebService hiloconexion;
+    EditText dni;
+    EditText nombre;
+    EditText monto;
+    EditText direccion;
+    TextView resultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +51,127 @@ public class RegistrarSolicitudActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        dni = (EditText)findViewById(R.id.eDNI);
+        nombre = (EditText)findViewById(R.id.eNombre);
+        monto = (EditText)findViewById(R.id.eMonto);
+        direccion = (EditText)findViewById(R.id.eDireccion);
+        resultado = (TextView)findViewById(R.id.resultado);
+    }
+
+    public void InsertarSolicitud(View v) {
+        hiloconexion = new ObtenerWebService();
+        hiloconexion.execute(INSERT,"3",dni.getText().toString(),nombre.getText().toString(),monto.getText().toString(),direccion.getText().toString());   // Parámetros que recibe doInBack
+    }
+
+
+    public class ObtenerWebService extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String cadena = params[0];
+            URL url = null; // Url de donde queremos obtener información
+            String devuelve = "";
+
+
+            if (params[1] == "3") {    // insert
+
+                try {
+                    HttpURLConnection urlConn;
+
+                    DataOutputStream printout;
+                    DataInputStream input;
+                    url = new URL(cadena);
+                    urlConn = (HttpURLConnection) url.openConnection();
+                    urlConn.setDoInput(true);
+                    urlConn.setDoOutput(true);
+                    urlConn.setUseCaches(false);
+                    urlConn.setRequestProperty("Content-Type", "application/json");
+                    urlConn.setRequestProperty("Accept", "application/json");
+                    urlConn.connect();
+                    //Creo el Objeto JSON
+                    JSONObject jsonParam = new JSONObject();
+                    jsonParam.put("dni", params[2]);
+                    jsonParam.put("nombre", params[3]);
+                    jsonParam.put("monto", params[4]);
+                    jsonParam.put("direccion", params[5]);
+
+                    // Envio los parámetros post.
+                    OutputStream os = urlConn.getOutputStream();
+                    BufferedWriter writer = new BufferedWriter(
+                            new OutputStreamWriter(os, "UTF-8"));
+                    writer.write(jsonParam.toString());
+                    writer.flush();
+                    writer.close();
+
+                    int respuesta = urlConn.getResponseCode();
+
+
+                    StringBuilder result = new StringBuilder();
+
+                    if (respuesta == HttpURLConnection.HTTP_OK) {
+
+                        String line;
+                        BufferedReader br = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+                        while ((line = br.readLine()) != null) {
+                            result.append(line);
+                            Log.i("mi  error", "[" + result + "]");
+                            //response+=line;
+                        }
+                        Log.i("mi  error", "[" + result + "]");
+                        //Creamos un objeto JSONObject para poder acceder a los atributos (campos) del objeto.
+                        JSONObject respuestaJSON = new JSONObject(result.toString());   //Creo un JSONObject a partir del StringBuilder pasado a cadena
+                        //Accedemos al vector de resultados
+
+                        String resultJSON = respuestaJSON.getString("estado");   // estado es el nombre del campo en el JSON
+
+                        if (resultJSON == "1") {      // hay un alumno que mostrar
+                            devuelve = "Solicitud insertado correctamente";
+
+                        } else if (resultJSON == "2") {
+                            devuelve = "Solicitud no pudo insertarse";
+                        }
+
+                    }
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                return devuelve;
+
+
+            }
+
+
+            return null;
+        }
+        @Override
+        protected void onCancelled(String s) {
+            super.onCancelled(s);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            resultado.setText(s);
+            //super.onPostExecute(s);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+
     }
 
 }
